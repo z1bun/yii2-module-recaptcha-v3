f4375f1260723902ac82063a7ce4a9644048368e
artem <artem@konstantinov.xyz>
2019-11-07 18:58:59 +0300

    Fix key

--- src/assets/js/recaptcha.js
+++ src/assets/js/recaptcha.js
@@ -3,7 +3,7 @@ var cptSiteKey = cptFirst.data('cpt-key');
 var cptAction = cptFirst.data('cpt-action');
 
 function resetRecaptchaToken() {
-    if (typeof siteKey !== 'undefined' && typeof cptAction !== 'undefined') {
+    if (typeof cptSiteKey !== 'undefined' && typeof cptAction !== 'undefined') {
         grecaptcha.execute(cptSiteKey, {action: cptAction}).then(function (token) {
             $('.jsCpt').each(function () {
                 $(this).val(token);
